0299fed0032b159377f3ae80a6eaf78983406f79
fengyuxiayin <JYliangyuan5h1>
2016-11-24 17:27:32 +0800

    init

---
+++ app/src/main/java/phone1000/com/adancedday14git/MainActivity.java
@@ -0,0 +1,13 @@
+package phone1000.com.adancedday14git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
